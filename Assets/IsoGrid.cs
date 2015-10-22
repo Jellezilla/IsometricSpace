d928f8644019dca8f9d7a3f488a35af4a2d4eb25
Jellezilla <jesper@tjorne.dk>
2015-10-23 00:02:09 +0200

    SpawnGrid() method

--- Assets/IsoGrid.cs
+++ Assets/IsoGrid.cs
@@ -4,19 +4,38 @@ using System.Collections;
 public class IsoGrid : MonoBehaviour {
 
     public Transform Tile;
-	// Use this for initialization
-	void Start () {
-        for (int x = 0; x < 16; x++)
+    public int size;
+    
+
+    private Vector3[,] spawnGrid = new Vector3[3, 4];
+
+    // Use this for initialization
+    void Start () {
+        SpawnGrid();
+    }
+	
+    void SpawnGrid()
+    {
+        for (int x = 0; x < size; x++)
         {
-            for (int y = 0; y < 16; y++)
+            for (int y = 0; y < size; y++)
             {
                 Instantiate(Tile, new Vector3(x, 0, y), Quaternion.identity);
+                            
             }
         }
     }
-	
+
+
+
 	// Update is called once per frame
 	void Update () {
-	  
+	  if(Input.GetKeyDown(KeyCode.O))
+        {
+            foreach (GameObject go in GameObject.FindGameObjectsWithTag("Tile"))
+                Destroy(go);
+
+            SpawnGrid();
+        }
 	}
 }
