5d499d0a83824b9418fe84a55d02d4d63231cb7e
Jellezilla <jesper@tjorne.dk>
2015-10-22 18:06:00 +0200

    RemovedSkyBox+AddedDirs
    Added more directories

--- Assets/IsoGrid.cs
+++ Assets/IsoGrid.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class IsoGrid : MonoBehaviour {
 
+    public Transform Tile;
 	// Use this for initialization
 	void Start () {
-	
-	}
+        for (int x = 0; x < 16; x++)
+        {
+            for (int y = 0; y < 16; y++)
+            {
+                Instantiate(Tile, new Vector3(x, 0, y), Quaternion.identity);
+            }
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+	  
 	}
 }
