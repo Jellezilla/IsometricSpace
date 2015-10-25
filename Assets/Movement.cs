ac0105115a968627aeedf90249ed0a213ac6950c
Jellezilla <jesper@tjorne.dk>
2015-10-25 18:01:48 +0100

    added player capsule and alpha version movement
    also added a solarsystem just for shits and giggles.

---
+++ Assets/Movement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    Rigidbody rigid;
+    private float h, v;
+    private float maxSpeed;
+	// Use this for initialization
+	void Start () {
+        rigid = transform.GetComponent<Rigidbody>();
+        maxSpeed = 3.0F;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+    void FixedUpdate()
+    {
+        v = Input.GetAxis("Vertical");
+        h = Input.GetAxis("Horizontal");
+        
+        if (v > 0)
+        {
+            rigid.AddForce(GetIsoDir(Vector3.right), ForceMode.Impulse);
+        } else if (v < 0)
+        {
+            rigid.AddForce(GetIsoDir(Vector3.right) * -1, ForceMode.Impulse);
+        }
+
+        if(h < 0)
+        {
+            rigid.AddForce(GetIsoDir(Vector3.forward), ForceMode.Impulse);
+        } else if (h >   0)
+        {
+            rigid.AddForce(GetIsoDir(Vector3.forward) * -1, ForceMode.Impulse);
+        }
+
+        if (rigid.velocity.magnitude > maxSpeed)
+        {
+            rigid.velocity = rigid.velocity.normalized * maxSpeed;
+        }
+    }
+    public Vector3 GetIsoDir(Vector3 dir) {
+        dir = Quaternion.Euler(0, -45, 0) * dir;
+        return dir;
+    }
+}
---
+++ Assets/Orbit.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbit : MonoBehaviour {
+
+    public float distanceToStar;
+    public float orbitSpeed;
+    public float rotationSpeed;
+    
+    private Transform orbitAround;
+
+	// Use this for initialization
+	void Start () {
+        orbitAround = transform.parent;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void FixedUpdate()
+    {
+        transform.RotateAround(orbitAround.transform.position, Vector3.up, orbitSpeed * Time.deltaTime);
+
+    }
+}
