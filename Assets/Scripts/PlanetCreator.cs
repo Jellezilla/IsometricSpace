06337e715aac7201d57c07f526b2311b0cc36525
Jellezilla <jesper@tjorne.dk>
2015-11-11 12:45:04 +0100

    Main menu and planet scene added

---
+++ Assets/Scripts/PlanetCreator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetCreator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		InitializeSurfaceMaterials ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void InitializeSurfaceMaterials() {
+
+	}
+}
