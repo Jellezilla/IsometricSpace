276f725d414ab66f3991c0e6bba699592b005666
Jellezilla <jesper@tjorne.dk>
2015-12-01 15:23:12 +0100

    mouselook is now working properly.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -89,20 +89,32 @@ public class Movement : MonoBehaviour {
     }
 
 	void ControlMouse() {
-		
-		Vector3 mousePos = Input.mousePosition;
-		mousePos = cam.ScreenToWorldPoint(new Vector3(mousePos.x,mousePos.y,cam.transform.position.y - transform.position.y));
-		targetRotation = Quaternion.LookRotation(mousePos - new Vector3(transform.position.x,0,transform.position.z));
 
-		transform.eulerAngles = GetIsoDir(Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y,targetRotation.eulerAngles.y,rotationSpeed * Time.deltaTime));
+		float speed = 30.0f;
+		Plane playerPlane = new Plane(Vector3.up, transform.position);
 		
-		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
-		Vector3 motion = input;
-		motion *= (Mathf.Abs(input.x) == 1 && Mathf.Abs(input.z) == 1)?.7f:1;
-		//motion *= (Input.GetButton("Run"))?runSpeed:walkSpeed;
-		motion += Vector3.up * -8;
+		// Generate a ray from the cursor position
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 		
-		//controller.Move(motion * Time.deltaTime);
+		// Determine the point where the cursor ray intersects the plane.
+		// This will be the point that the object must look towards to be looking at the mouse.
+		// Raycasting to a Plane object only gives us a distance, so we'll have to take the distance,
+		//   then find the point along that ray that meets that distance.  This will be the point
+		//   to look at.
+		float hitdist = 0.0f;
+		// If the ray is parallel to the plane, Raycast will return false.
+		if (playerPlane.Raycast (ray, out hitdist)) 
+		{
+			// Get the point along the ray that hits the calculated distance.
+			Vector3 targetPoint = ray.GetPoint(hitdist);
+			
+			// Determine the target rotation.  This is the rotation if the transform looks at the target point.
+			Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+			
+			// Smoothly rotate towards the target point.
+			transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+		}
 
+	
 	}
 }
